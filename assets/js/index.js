b7a710043801e6132a058aec22d26348df6229df
rubensjuniors <rubensjunio2110@gmail.com>
2023-02-10 17:35:10 -0300

    adicionado clicks aos cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,90 +1,27 @@
-// const classActive = 'ativo';
-// const cards = document.querySelectorAll(".ops-item");
-// const cardsPickeds = document.querySelectorAll('.ops-card-item img');
+const classActive = "ativo";
+const elementCards = document.querySelectorAll(".card-element");
+const buttonStart = document.querySelector('#start');
+const elementCardspicked = document.querySelectorAll(".selected-card-element ");
 
-// const btnStart = document.querySelector('#start');
-// const gameCorpo = document.querySelector(".corpo-game");
-// const startGame = document.querySelector(".start-game");
+function handleCardClick(cardItem){
+  const cardImagemAlt = this.children[0].getAttribute("alt");
 
-// function pickedCardsClick(){
-
-// function disableCard(cards, index, remove){
-//   remove && cards.forEach((content)=>{
-//     content.classList.remove(classActive);
-//   });
-
-//   cards[index].classList.add(classActive)
-// console.log(cards, index, remove)
-// }
-
-// function selecionarCard(cardItem, index){
-//   cardItem.addEventListener("click", () => {
-//     disableCard(cardsPickeds, index, true);
-//   });
-// }
-
-// cards.forEach(selecionarCard);
-// disableCard(cardsPickeds, 5)
-// }
-// pickedCardsClick();
-
-// function pickedCardsHover(){
-
-// function hoverPicked(event){
-// const cards = event.currentTarget.getAttribute("id");
-
-// cardsPickeds.forEach((cardPicked)=>{
-//     const alt = cardPicked.getAttribute("alt");
-//     cardPicked.classList.remove(classActive);
-//     if(cards === alt){
-//       cardPicked.classList.add(classActive);
-//     }
-// });
-// }
-// cards.forEach((card)=>{
-//   card.addEventListener("mouseout", hoverPicked)
-// })
-// }
-// pickedCardsHover();
-
-// function startGameActive(){
-
-// function hiddenPicked(event){
-//   gameCorpo.style.display = "none"
-//   startGame.style.display = "none"
-// }
-
-// btnStart.addEventListener('click', hiddenPicked)
-// }
-// startGameActive();
-
-
-const activeClass = 'ativo';
-const cardElements = document.querySelectorAll(".ops-item");
-const pickedCardImages = document.querySelectorAll('.ops-card-item img');
-
-const startButton = document.querySelector('#start');
-const gameBody = document.querySelector(".corpo-game");
-const startScreen = document.querySelector(".start-game");
-
-cardElements.forEach((cardElement, index) => {
-  cardElement.addEventListener("click", () => {
-    pickedCardImages.forEach(img => img.classList.remove(activeClass));
-    pickedCardImages[index].classList.add(activeClass);
+ 
+  elementCardspicked.forEach((cardPicked)=>{
+    const teste = cardPicked.getAttribute('id')
+    cardPicked.classList.remove("ativo")
+    if(cardImagemAlt === teste){
+      cardPicked.classList.add("ativo")
+      console.log('ppoas')
+    }
   });
-  cardElement.addEventListener("mouseout", ({ currentTarget }) => {
-    const id = currentTarget.getAttribute("id");
-    pickedCardImages.forEach(img => {
-      if (img.getAttribute("alt") === id) {
-        img.classList.add(activeClass);
-      } else {
-        img.classList.remove(activeClass);
-      }
-    });
-  });
-});
 
-startButton.addEventListener('click', () => {
-  gameBody.style.display = "none";
-  startScreen.style.display = "none";
+
+
+  
+console.log(cardImagemAlt)
+}
+
+elementCards.forEach((cardElement)=>{
+  cardElement.addEventListener('click', handleCardClick)
 });
