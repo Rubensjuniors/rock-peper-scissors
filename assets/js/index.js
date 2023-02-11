1001f1b9b800dcccca1c7ad14d665a814b490829
rubensjuniors <rubensjunio2110@gmail.com>
2023-02-11 09:15:42 -0300

    feito click dos buttons

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,17 @@
 const activeClass = "ativo";
-const telaSmall = window.matchMedia("(max-width: 700px)");
+
+const primarySection = document.querySelector(".primary-section");
+const secondarySection = document.querySelector(".secondary-section");
+
 const cardElements = document.querySelectorAll(".card-element");
-const startButton = document.querySelector('#start');
 const pickedCardElements = document.querySelectorAll(".selected-card-element");
+const cardPickedSelected = document.querySelector(".card-picked");
+const rulesModal = document.querySelector(".modal-rules");
+
+const startButton = document.querySelector('#start');
+const playAgainButton = document.querySelector("#play-again");
+const rulesButton = document.querySelector("#button-rules");
+const buttonFecharModal = document.querySelector("#fechar");
 
 
 function handleCardClick(){
@@ -11,20 +20,34 @@ function handleCardClick(){
     pickedCard.classList.remove(activeClass);
     if (cardAlt === pickedCard.getAttribute('id')) {
       pickedCard.classList.add(activeClass);
-      console.log(pickedCard)
+      cardPickedSelected.innerHTML = pickedCard.innerHTML
     }
   });
 
 }
 
 cardElements.forEach((card) => {
-  if(telaSmall){
-    card.addEventListener('touchstart', handleCardClick);
-  }
     card.addEventListener('click', handleCardClick);
+    card.addEventListener('touchstart', handleCardClick);
     card.addEventListener('mouseover', handleCardClick);
 
 });
 
+startButton.addEventListener("click",()=>{
+primarySection.style.display = "none";
+secondarySection.style.display = "grid";
+})
 
-console.log(pickedCardElements)
+playAgainButton.addEventListener("click", ()=>{
+  primarySection.style.display = "flex";
+  secondarySection.style.display = "none";
+})
+
+rulesButton.addEventListener("click",()=>{
+  rulesModal.style.display = "flex";
+})
+
+buttonFecharModal.addEventListener("click",()=>{
+  rulesModal.style.display = "none";
+
+})
