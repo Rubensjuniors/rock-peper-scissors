2228075ecf3715f978610cf6cf79169df0274203
rubensjuniors <rubensjunio2110@gmail.com>
2023-02-11 22:05:43 -0300

    algumas refatorações no codigo js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,53 +1,77 @@
 const activeClass = "ativo";
+const hide = 'none';
 
 const primarySection = document.querySelector(".primary-section");
 const secondarySection = document.querySelector(".secondary-section");
 
 const cardElements = document.querySelectorAll(".card-element");
-const pickedCardElements = document.querySelectorAll(".selected-card-element");
+const pickedCardElements = Array.from(document.querySelectorAll(".selected-card-element"));
 const cardPickedSelected = document.querySelector(".card-picked");
 const rulesModal = document.querySelector(".modal-rules");
+const quetionMark = document.querySelector("#quetion");
 
 const startButton = document.querySelector('#start');
 const playAgainButton = document.querySelector("#play-again");
 const rulesButton = document.querySelector("#button-rules");
-const buttonFecharModal = document.querySelector("#fechar");
+const closeModalbutton = document.querySelector("#fechar");
 
-
-function handleCardClick(){
+function handleSelectedCard(){
+  // variavel que pega os alt dos dos cards
   const cardAlt = this.children[0].getAttribute("alt");
+
+  // loop das lista de imagem escolhidas 
   pickedCardElements.forEach((pickedCard) => {
+    // remoce a class ativo pra ficar so um item ativo
     pickedCard.classList.remove(activeClass);
+
+    // if que verifica se os alt das imagems são iguais as dos ids
     if (cardAlt === pickedCard.getAttribute('id')) {
+      // add uma class ativo correspondente ao item clicado
       pickedCard.classList.add(activeClass);
+
+      // add o card escolhido na outra parte do game
       cardPickedSelected.innerHTML = pickedCard.innerHTML
+
+      // button de jogar novamente
+      playAgainButton.addEventListener("click", ()=>{
+        
+        // esconde a segunda parte do game e mostra a primeira parte
+        primarySection.style.display = "flex";
+        secondarySection.style.display = hide;
+        
+        // remove a class ativo do card escolhido
+        pickedCard.classList.remove(activeClass);
+
+        // if que verifica se não esta com ativo e add o question mark novamente e remove o card escolhido 
+        if(pickedCard.classList.contains(activeClass) !== activeClass){
+          quetionMark.classList.add(activeClass);
+          cardPickedSelected.children[0].remove();
+        }
+      });
     }
   });
-
 }
-
+// loop pelas opções dos cards
 cardElements.forEach((card) => {
-    card.addEventListener('click', handleCardClick);
-    card.addEventListener('touchstart', handleCardClick);
-    card.addEventListener('mouseover', handleCardClick);
-
+    card.addEventListener('click', handleSelectedCard);
 });
 
+// button de start
 startButton.addEventListener("click",()=>{
-primarySection.style.display = "none";
-secondarySection.style.display = "grid";
-})
+  // if que para se nem um card for escolhido
+  if(!cardPickedSelected.children[0]){
+   return;
+  }
+  // se card for escolhido esconde primeira parte e mostra a segunda
+  primarySection.style.display = hide;
+  secondarySection.style.display = "grid";
+});
 
-playAgainButton.addEventListener("click", ()=>{
-  primarySection.style.display = "flex";
-  secondarySection.style.display = "none";
-})
-
+// button das regras mostra modal
 rulesButton.addEventListener("click",()=>{
   rulesModal.style.display = "flex";
-})
-
-buttonFecharModal.addEventListener("click",()=>{
-  rulesModal.style.display = "none";
-
-})
+});
+// button de fechar modal
+closeModalbutton.addEventListener("click",()=>{
+  rulesModal.style.display = hide;
+});
